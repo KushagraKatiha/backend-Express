26dd19d53aea652eb2b26675d340f886d441712c
Kushagra Katiha <116267840+KushagraKatiha@users.noreply.github.com>
2023-09-28 01:46:46 +0530

    handling routes

--- server.js
+++ server.js
@@ -3,7 +3,28 @@ const http = require('http')
 const PORT = 5000
 
 const server = http.createServer((req, res)=>{
-    res.end("Welcome to my Code !!")
+    if(req.url === '/'){
+        res.statusCode='200'    // Set the response_code
+        res.setHeader("Content-Type", "text/plain")       // To  set the content type
+    
+        res.end(JSON.stringify({"HEllo": "Kushagra"}))
+    }else if (req.url === '/about'){
+        res.statusCode='200'    // Set the response_code
+        res.setHeader("Content-Type", "text/plain")       // To  set the content type
+    
+        res.end(JSON.stringify({"name": "Kushagra"}))
+    }else if (req.url === '/mentor'){
+        res.statusCode='200'    // Set the response_code
+        res.setHeader("Content-Type", "text/plain")       // To  set the content type
+    
+        res.end(JSON.stringify({"mentor": "Kushagra"}))
+    }else{
+        res.statusCode='500'    // Set the response_code
+        res.setHeader("Content-Type", "text/plain")       // To  set the content type
+    
+        res.end('Error Occured')
+    }
+   
 })
 
 server.listen(PORT, ()=>{
